f28627e0497fbfbcb07fc2351fbe988f4dfcfecc
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-08-15 01:41:28 +0100

    Update Star.js

--- Creatures/Star.js
+++ Creatures/Star.js
@@ -151,7 +151,7 @@ function createStars() {
   for (let i = 0; i < numStars; i++) {
     let centerX = random(width / 6, width - width / 6);
     let centerY = random(height / 6, height - height / 6);
-    angStars.push(random(3, 7));
+    angStars.push(floor(random(3, 8)));
     let innerRadius = random(10, 20);
     let outerRadius = innerRadius + random(10, 30);
     let star = new Star(centerX, centerY, angStars[i], innerRadius, outerRadius);
