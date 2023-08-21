02e2a30d77cd3e2a12ca766d93aa62adfd34e86b
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-08-21 13:29:54 +0100

    add local hand files

--- handTracking/detection.js
+++ handTracking/detection.js
@@ -1,12 +1,16 @@
 let detections ={};
 const videoElement = document.getElementById('video');
+//const canvasElement = document.getElementByClass("canvas");
 
 function gotHands(results) {
   detections = results;
 }
 
+// const hands = new Hands({locateFile: (file) => {
+//   return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+// }});
 const hands = new Hands({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  return `./libraries/mediapipe-hands/${file}`;
 }});
 hands.setOptions({
   maxNumHands: 1, // the max number of hands
@@ -22,9 +26,14 @@ const camera = new Camera(videoElement, {
   onFrame: async () => {
     await hands.send({image: videoElement});
   },
-  // width: 2400/5,
-  // height: 2400
-  // width: window.innerWidth,
-  // height: window.innerHeight
+  width: 2400/4,
+  height: 2400
+  // width: canvasElement.width,
+  // height: canvasElement.height
 });
 camera.start();
+
+// videoElement.width = canvas.width;
+// videoElement.height = canvas.height;
+// videoElement.style.width = canvas.width;
+// videoElement.style.height = canvas.height;
