5ec1e9860e78e5794fc51d5cc121dbaa5ac193dc
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-08-21 01:43:23 +0100

    use GPU

--- handTracking/detection.js
+++ handTracking/detection.js
@@ -12,7 +12,8 @@ hands.setOptions({
   maxNumHands: 1, // the max number of hands
   modelComplexity: 1, //maybe change to 0
   minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
+  minTrackingConfidence: 0.5,
+  delegate: "GPU"
 });
 hands.onResults(gotHands);
 
@@ -21,8 +22,8 @@ const camera = new Camera(videoElement, {
   onFrame: async () => {
     await hands.send({image: videoElement});
   },
-  width: 2400/5,
-  height: 2400
+  // width: 2400/5,
+  // height: 2400
   // width: window.innerWidth,
   // height: window.innerHeight
 });
